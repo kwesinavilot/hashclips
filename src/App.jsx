af0f93b1b0f01bc2442b78f8157cf968656d72b2
Kwesi Navilot <andrewskwesiankomahene@gmail.com>
2024-07-27 19:07:58 +0000

    (fix) added central nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
-import './styles/index.css'
+import { useState } from 'react';
+import './styles/index.css';
+import Heading from "@/components/site/Heading";
 
 function App() {
   return (
-    <>
-      <p>Hello World</p>
-    </>
-  )
+    <div className="app">
+      <Heading />
+
+      {/* <main className="app-main">
+        <p>Hello World</p>
+      </main>
+
+      <footer className="app-footer">
+        <p>&copy; 2024 HashClips. All rights reserved.</p>
+      </footer> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
